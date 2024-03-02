028f4aeb407f602e7552589c80cb3b4e6cb9ed3d
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-03-02 17:30:39 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import io
+from PIL import Image
+
+API_URL = "https://api-inference.huggingface.co/models/stablediffusionapi/duchaiten-real3d-nsfw-xl"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.content, response.url
+
+image_bytes, download_url = query({
+    "inputs": "Astronaut riding a horse",
+})
+
+try:
+    # Attempt to open the image with PIL
+    image = Image.open(io.BytesIO(image_bytes))
+
+    # Save the image to the current working directory
+    filename = "downloaded_image.png"
+    image.save(filename)
+
+except PIL.UnidentifiedImageError as e:
+    print(f"Error opening image: {e}")
+except Exception as e:
+    print(f"Error: {e}")
